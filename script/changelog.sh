d288a4028f3a2014e22e9f29c5b3a4b5f89dc5a4
Kunal Memane <kmemane@redhat.com>
2024-10-25 13:26:47 +0530

    ci: Main branch cicd added

--- script/changelog.sh
+++ script/changelog.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
-#
-# This script generates changelog for each releases
 
-NEW_TAG=$(git describe --tags --abbrev=0)
-OLD_TAG=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1))
+TAGS=($(git tag --sort=-creatordate | head -n 2))
+LATEST_TAG="${TAGS[0]}"
+PREVIOUS_TAG="${TAGS[1]}"
 
-cat << EOF
-# Changelog
+if [ ${#TAGS[@]} -lt 2 ]; then
+    echo "Not enough tags found to generate changelog."
+    exit 1
+fi
 
-## [$(git describe --tags --abbrev=0)] - $(date +%d-%m-%Y)
-EOF
-for i in "feat:Added" "chore:Minor Changes" "test:Test" "docs:Documentation" "bug:Fixed" "refactor:Changed" "remove:Removed" "deprecate:Deprecated" "security:Security" "ci:CI"; do
-    TYPE=${i%%:*}
-    TITLE=${i#*:}
-    if [[ -n $(git log --pretty='%h - %s (%an)' ${OLD_TAG}..${NEW_TAG} | grep -i -E "^.{10}(${TYPE}: )") ]]; then
-        echo -e "\n### ${TITLE}\n"
-        git log --pretty="%h - %s (%an)" ${OLD_TAG}..${NEW_TAG} | grep -i -E "^.{10}(${TYPE}: )" | sed "s/${TYPE}: //"
-    fi
+CURRENT_DATE=$(date "+%Y-%m-%d")
+
+echo -e "\n# Changelog\n"
+echo -e "## Release Notes from $PREVIOUS_TAG to $LATEST_TAG\n"
+echo -e "### $CURRENT_DATE\n"
+
+declare -A tag_map=(
+    ["chore"]="Minor change"
+    ["docs"]="Document"
+    ["feat"]="Feature Added"
+    ["bug"]="Fixed"
+    ["ci"]="CI"
+    ["refactor"]="Changed"
+    ["test"]="Test"
+)
+
+# Collect and categorize commit messages
+for type in "${!tag_map[@]}"; do
+    
+    title="${tag_map[$type]}"
+
+    if git log "$PREVIOUS_TAG..$LATEST_TAG" --pretty='%h - %s (%an)' | grep -q "$type"; then
+        echo -e "\n### $title \n"
+    
+       git log "$PREVIOUS_TAG..$LATEST_TAG" --pretty='%h - %s (%an)' | awk -v type="$type" '$0 ~ "^.{10}(" type ": )" { sub(type ": ", ""); print }'
+    fi 
 done
+
+echo -e "\nChangelog generated for release $LATEST_TAG."
